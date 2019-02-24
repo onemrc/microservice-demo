819ab4c1c36cfcf91f4b6377387d38736c0c6bfe
onrmrc <1774122996@qq.com>
2019-02-24 14:34:36 +0800

    自定义HystrixConcurrencyStrategy实现，将父线程上下文注入Hystrix管理的线程中

---
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/utils/UserContextFilter.java
@@ -0,0 +1,47 @@
+package com.thoughtmechanix.licenses.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class UserContextFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(UserContextFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        //检索调用的HTTP首部中设置的值，将这些值赋给存储在UserContextHolder中的UserContext
+
+        UserContextHolder.getContext().setCorrelationId(httpServletRequest.getHeader(UserContext.CORRELATION_ID));
+        UserContextHolder.getContext().setUserId(httpServletRequest.getHeader(UserContext.USER_ID));
+        UserContextHolder.getContext().setAuthToken(httpServletRequest.getHeader(UserContext.AUTH_TOKEN));
+        UserContextHolder.getContext().setOrgId(httpServletRequest.getHeader(UserContext.ORG_ID));
+
+        logger.debug("UserContextFilter Correlation id: {}", UserContextHolder.getContext().getCorrelationId());    //不知道为什么不起作用
+        System.out.println("UserContextFilter Correlation id: " + UserContextHolder.getContext().getCorrelationId());
+
+        filterChain.doFilter(httpServletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
